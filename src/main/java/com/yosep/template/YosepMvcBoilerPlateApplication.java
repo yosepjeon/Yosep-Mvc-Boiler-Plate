a18ae9b962f9cfc39773479edd4fce681f4fb498
yosepjeon <enekelx1@naver.com>
2022-11-10 10:14:50 +0900

    spring server 세팅

---
+++ src/main/java/com/yosep/template/YosepMvcBoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package com.yosep.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosepMvcBoilerPlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YosepMvcBoilerPlateApplication.class, args);
+	}
+
+}
